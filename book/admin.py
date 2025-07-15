dc91872bab098aa3e433bc9d930b3966a7cbb1d9
ayumu <a.hosoda.sys24@morijyobi.ac.jp>
2025-07-15 15:12:58 +0900

    Djangoの課題

--- book/admin.py
+++ book/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Shelf
+from .models import Shelf, Review
 # Register your models here.
 
 
 
 admin.site.register(Shelf)
+admin.site.register(Review)
--- book/models.py
+++ book/models.py
@@ -1,24 +1,38 @@
 from django.db import models
+from .consts import MAX_RATE
+
+
+RATE_CHOICES = [(x, str(x)) for x in range(0, MAX_RATE + 1)]
 
 CATEGORY = (
-    ('business', 'ビジネス'),
-    ('life', '生活'),
-    ('hobby', '趣味'),
-    ('comic', '漫画'),
-    ('other', 'その他'),
+    ('ビジネス', 'ビジネス'),
+    ('生活', '生活'),
+    ('趣味', '趣味'),
+    ('漫画', '漫画'),
+    ('その他', 'その他'),
 )
 
 class Shelf(models.Model):
     title = models.CharField(max_length=100)
     text = models.TextField()
+    
     category = models.CharField(
         max_length=100,
         choices = CATEGORY,
         )
-
+    thumbnail = models.ImageField(null=True, blank=True)
     def __str__(self):
         return self.title
 
+class Review(models.Model):
+    book = models.ForeignKey(Shelf, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+    rate = models.IntegerField(choices=RATE_CHOICES)
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.title
 
 # Create your models here.
 
