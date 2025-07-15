d46a0e03a0af04654b3322c5c39623ab33a17ed2
ayumu <a.hosoda.sys24@morijyobi.ac.jp>
2025-07-15 14:00:52 +0900

    Djangoの課題

---
+++ book/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Shelf
+# Register your models here.
+
+
+
+admin.site.register(Shelf)
---
+++ book/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+CATEGORY = (
+    ('business', 'ビジネス'),
+    ('life', '生活'),
+    ('hobby', '趣味'),
+    ('comic', '漫画'),
+    ('other', 'その他'),
+)
+
+class Shelf(models.Model):
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+    category = models.CharField(
+        max_length=100,
+        choices = CATEGORY,
+        )
+
+    def __str__(self):
+        return self.title
+
+
+# Create your models here.
+
+
